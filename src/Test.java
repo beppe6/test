e24df9472a8108f23487ce649a011a197033cbc7
beppe6 <giuseppe87@tin.it>
2015-03-08 16:12:20 +0100

    Test

---
+++ src/Test.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList <Integer> array= new ArrayList<Integer>();
+		
+		array.add(2);
+		array.add(0);
+		array.add(0);
+		array.add(0);
+		array.add(0);
+		array.add(0);
+		array.add(0);
+		array.add(1);
+		
+		int obs=5;
+		
+		int total=0;
+		double std=0;
+		
+		for(Integer i:array)
+			total+=i;
+		
+		total/=array.size();
+		
+		for(Integer i:array)
+		std+=(i-total)*(i-total);
+		
+		std/=array.size();
+		
+		std=Math.sqrt(std);
+		
+		System.out.println((obs-total)/std);
+		
+	}
+
+}
