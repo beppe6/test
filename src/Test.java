a86a3652732ec71f276edb0a2756ad8815d789a6
beppe6 <giuseppe87@tin.it>
2015-03-12 00:15:06 +0100

    one

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubj
 		
 		ArrayList <Integer> array= new ArrayList<Integer>();
 		
